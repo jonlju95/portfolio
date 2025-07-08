9bb20c65661c82b24917cad92ef3250f36f9c98d
Jonatan Ljung <jonatan.ljung@hotmail.com>
2025-07-08 13:17:20 +0200

    Remove translation

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,7 +1,7 @@
 import { getLocaleFromNavigator, init, register } from 'svelte-i18n';
 
-register('en', () => import('./translations/en.json'));
-register('sv', () => import('./translations/se.json'));
+// register('en', () => import('./translations/en.json'));
+// register('sv', () => import('./translations/se.json'));
 
 export function setupI18n(initialLocale?: string) {
 	init({
